0c0931b3672c369316cc832c81ccfb501a9422e4
CristianDC27 <cristiancasado27@gmail.com>
2020-11-27 22:08:02 +0100

    filtros matriculacion

--- php/matriculacion.php
+++ php/matriculacion.php
@@ -1,24 +1,4 @@
 <?php
-$errores = array();
-if (!preg_match("/^[a-zA-Z]+/", $_POST['name'])) {
-  $errores[] = "Sólo se permiten letras como nombre <br>";
-}
-if (strlen($_POST["dni"]) != 9) {
-  $errores[] = "El formato de dni es incorrecto";
-}
-
-if (!preg_match("/^[a-zA-Z]+/", $_POST["parent"])) {
-  $errores[] = "Sólo se permiten letras como nombre <br>";
-}
-if (!preg_match("/^[1-9]+/", $_POST["whatsapp"])) {
-  $errores[] = "Sólo se permiten dígitos en el telefono <br>";
-  if (strlen($_POST["whatsapp"]) != 9) {
-    $errores[] = "Se debe introducir el teléfono con 9 dígitos <br>";
-  }
-}
-if (!preg_match("/^[a-zA-Z]+/", $_POST["allergies"]) && !empty($_POST["allergies"])) {
-  $errores[] = "Sólo se permiten letras en las alergias <br>";
-}
 if (empty($errores)) {
   $nombre = filtrado($_POST["name"]);
   $fechaNac = filtrado($_POST["birthdate"]);
@@ -31,8 +11,4 @@ if (empty($errores)) {
   $justificante = $_POST["payment"];
   $pagado = empty($justificante);
   echo ("Correcto");
-} else {
-  foreach ($errores as $error) {
-    echo ("<li> $error </li>");
-  }
 }

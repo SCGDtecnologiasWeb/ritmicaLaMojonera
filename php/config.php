dffbb350dff29497329742eafabeb73c331299b9
CristianDC27 <cristiancasado27@gmail.com>
2020-11-28 12:19:20 +0100

    Insercion base de datos matriculacion

--- php/config.php
+++ php/config.php
@@ -7,5 +7,6 @@ $link = mysqli_connect($host, $user, $password, $database);
 
 if ($link->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $link->connect_errno . ") " . $link->connect_error . "<br/>";
+} else {
+    echo "Conectado " . $link->host_info . "<br/>";
 }
-echo "Conectado " . $link->host_info . "<br/>";
--- php/matriculacion.php
+++ php/matriculacion.php
@@ -1,14 +1,26 @@
 <?php
-if (empty($errores)) {
-  $nombre = filtrado($_POST["name"]);
-  $fechaNac = filtrado($_POST["birthdate"]);
-  $dni = filtrado($_POST["dni"]);
-  $tutor = filtrado($_POST["parent"]);
-  $telf = filtrado($_POST["whatsapp"]);
-  $nivel = filtrado($_POST["level"]);
-  $alergias = filtrado($_POST["allergies"]);
-  $consentimiento = filtrado($_POST["consent"]);
-  $justificante = $_POST["payment"];
-  $pagado = empty($justificante);
-  echo ("Correcto");
+include("funciones.php");
+
+//Parseamos las variables
+$nombre = filtrado($_POST["name"]);
+$fechaNac = filtrado($_POST["birthdate"]);
+$dni = filtrado($_POST["dni"]);
+$tutor = filtrado($_POST["parent"]);
+$telf = filtrado($_POST["whatsapp"]);
+$nivel = filtrado($_POST["level"]);
+$alergias = filtrado($_POST["allergies"]);
+$consentimiento = filtrado($_POST["consent"]);
+$justificante = $_POST["payment"];
+$pagado = (empty($justificante) ? "No" : "Si");
+
+//Conectamos a la base de datos
+require_once("config.php");
+//Creamos el código para insertar
+$sql = "REPLACE INTO Gimnasta (dni,nombreCompleto,fechaNacimiento,nombreTutor,telefono,nivel,consentimientoFotos,alergias,pago,fotoPago,registrado) VALUES ($dni,$nombre,$fechaNac,$tutor,$telf,$nivel,$consentimiento,$alergias,$pagado,$justificante,FALSE)";
+//Ejecutamos
+if (mysqli_query($link, $sql)) {
+  header("location: /html/index.html");
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($link);
 }
+mysqli_close($link);

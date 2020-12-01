5f2385b30b6e7ff15f83de11274ea9279d201c10
CristianDC27 <cristiancasado27@gmail.com>
2020-12-01 13:07:51 +0100

    nueva base de datos

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "1234";
-$database = "mydb";
+$password = "SergioCristianGonzaloDiego2020";
+$database = "ritmica";
 $link = mysqli_connect($host, $user, $password, $database);
 
 if ($link->connect_errno) {

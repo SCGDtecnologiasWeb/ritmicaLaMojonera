c544676b67d7e0a43e0c4512e88746fef9b3fe3f
Sergio Mesas <mesasyelamos@gmail.com>
2020-12-03 23:18:22 +0100

    logout

--- php/login.php
+++ php/login.php
@@ -1,46 +1,31 @@
 <?php
 include("funciones.php");
-$alert = '';
+require_once("config.php");
+if (empty($_SESSION))
+  session_start();
+if (!isset($_POST['continuar'])) {
+  header("Location: login.php");
+  exit;
+}
 $usuario = filtrado($_POST["usuario"]);
 $contraseña = filtrado($_POST["contraseña"]);
-session_start();
-require_once("config.php");
-$queryAdmin = mysqli_query($link, "SELECT * FROM Administrador WHERE correoAdmin ='$usuario'AND claveAccesoAdmin ='$contraseña'");
+
+$queryAdmin = mysqli_query($link, "SELECT * FROM Administrador WHERE correoAdmin ='$usuario'");
 $queryEntrenador = mysqli_query($link, "SELECT * FROM Entrenador WHERE correoEntrenador ='$usuario'AND claveAcceso ='$contraseña'");
 
 $resultAdmin = mysqli_num_rows($queryAdmin);
 $resultEntrenador = mysqli_num_rows($queryEntrenador);
 
-if (isset($_POST["continuar"])) {
-  //AQUI SI ENTRA
-  if (strlen($resultAdmin) > 0) {
-    //AQUI NO ENTRA
-    //echo "AAAAA";
-
-    while ($resultAdmin = mysqli_fetch_assoc($queryAdmin)) {
-      $bdUsuarioAdmin = $resultAdmin['correoAdmin'];
-      $bdClaveAdmin = $resultAdmin['claveAccesoAdmin'];
+if ($resultAdmin($queryAdmin) == 0) {
+  echo "NO ES VALIDO EL USUARIO";
+} else {
+  while ($row_query = mysqli_fetch_array($queryAdmin)) {
+    if ($row_query['contraseña'] == $contraseña) {
+      $_SESSION['contraseña'] = $_POST['contraseña'];
+      header("Location: matriculacion.php");
+      exit;
+    } else {
+      echo "CONTRASEÑA NO VALIDA";
     }
-    if ($usuario == $bdUsuarioAdmin && $contraseña == $bdClaveAdmin) {
-
-      $_SESSION['administrador'] = $usuario;
-      header("location: administrar_usuarios.php");
-    }
-  } else  if (strlen($resultEntrenador) > 0) {
-    //AQUI NO ENTRA
-    echo "AAAAA";
-
-    while ($resultEntrenador = mysqli_fetch_assoc($queryEntrenador)) {
-      $bdUsuarioEntrenador = $resultEntrenador["correoEntrenador"];
-      $bdClaveEntrenador = $resultEntrenador["claveAcceso"];
-    }
-    if ($usuario == $bdUsuarioEntrenador && $contraseña == $bdClaveEntrenador) {
-
-      $_SESSION["entrenador"] = $usuario;
-      header("location: /html/palmares.html");
-    }
-  } else {
-    $mensajeaccesoincorrecto = "El usuario y la contraseña son incorrectos, por favor vuelva a introducirlos.";
-    echo $mensajeaccesoincorrecto;
   }
 }

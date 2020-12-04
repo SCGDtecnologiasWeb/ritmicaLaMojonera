d3b3bd5ef6bf4bbdd1c2eadd0d1f96bdadea4a16
CristianDC27 <cristiancasado27@gmail.com>
2020-12-04 13:28:01 +0100

    filtrado arreglo

--- php/funciones.php
+++ php/funciones.php
@@ -4,6 +4,5 @@ function filtrado($datos)
     $datos = trim($datos); // Elimina espacios antes y después de los datos
     $datos = stripslashes($datos); // Elimina backslashes \
     $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
-    $datos = md5($datos);
     return $datos;
 }

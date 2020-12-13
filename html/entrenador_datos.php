ca6a966477d2df81c7252c4ea283e52f380ea391
CristianDC27 <cristiancasado27@gmail.com>
2020-12-13 19:39:37 +0100

    datos prueba

--- html/entrenador_datos.php
+++ html/entrenador_datos.php
@@ -42,60 +42,54 @@
   include_once($_SERVER['DOCUMENT_ROOT'] . '/php/config.php');
   $sql = "SELECT * FROM `Entrenador` WHERE idEntrenador= $idEntrenador";
   $resultado = $link->query($sql);
+  $link->close();
+  ?>
 
-  echo
-    "<!-- Content Start -->
-  <div class=\"main\">
-    <div class=\"content-container\">
+  <!-- Content Start -->
+  <div class="main">
+    <div class="content-container">
       <h1>Modificar datos de entrenador</h1>
-      <div class=\"form-container\">
-        <form action=\"/php/entrenador_datos.php\" method=\"POST\">
-          <div class=\"form-field\">
-            <label for=\"email\" class=\"field-title\">Correo electronico *</label><br />
-            <input type=\"email\" value=\"{$resultado["correoEntrenador"]}\" id=\"email\" name=\"email\" pattern=\"([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})\" required />
-            <input typer=\"text\" hidden value=\"$idEntrenador\" id=\"idEntrenador\" name=\"idEntrenador\"><br />
-          </div>
-          <div class=\"form-field\">
-            <label for=\"password\" class=\"field-title\">Contraseña *</label><br />
-            <input type=\"text\" value=\"{$resultado["claveAcceso"]}\" id=\"password\" name=\"password\" pattern=\"([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})\" required /><br />
+      <div class="form-container">
+        <form action="/php/entrenador_datos.php" method="POST">
+          <div class="form-field">
+            <label for="email" class="field-title">Correo electronico *</label><br />
+            <textarea type="email" id="email" name="email" pattern="([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})" required><?php$resultado["correoEntrenador"]?></textarea>
+            <input typer="text" hidden value="$idEntrenador" id="idEntrenador" name="idEntrenador"><br />
           </div>
 
-          <div class=\"form-field\">
-            <label for=\"name\" class=\"field-title\">Nombre completo</label><br />
-            <input type=\"text\" value=\"{$resultado["nombreCompleto"]}\" id=\"name\" name=\"name\" autocomplete=\"off\" pattern=\"[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,96}\" /><br />
+          <div class="form-field">
+            <label for="name" class="field-title">Nombre completo</label><br />
+            <textarea type="text" id="name" name="name" autocomplete="off" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,96}"><?php$resultado["nombreCompleto"]?></textarea><br />
           </div>
 
-          <div class=\"form-field\">
-            <label for=\"dni\" class=\"field-title\">DNI/NIE</label><br />
-            <input type=\"text\" value=\"{$resultado["DNI"]}\" id=\"dni\" name=\"dni\" autocomplete=\"off\" pattern=\"((([x-zX-Z])|([lmLM])|[0-9]){1}([ ]?)(([0-9]){7})([-]?)([a-zA-Z]{1}))\" minlength=\"9\" maxlength=\"11\" /><br />
+          <div class="form-field">
+            <label for="dni" class="field-title">DNI/NIE</label><br />
+            <textarea type="text" id="dni" name="dni" autocomplete="off" pattern="((([x-zX-Z])|([lmLM])|[0-9]){1}([ ]?)(([0-9]){7})([-]?)([a-zA-Z]{1}))" minlength="9" maxlength="11"><?php$resultado["DNI"]?></textarea><br />
           </div>
 
-          <div class=\"form-field\">
-            <label for=\"whatsapp\" class=\"field-title\">Teléfono (Whatsapp)</label><br />
-            <script src=\"/js/main.js\"></script>
-            <label id=\"tlf-prefijo\">
-              <input type=\"text\" value=\"{$resultado["telefono"]}\" id=\"whatsapp\" name=\"whatsapp\" class=\"ibacor_fi\" data-prefix=\"\" autocomplete=\"off\" pattern=\"(((([6]{1})([0-9]{2}))|(([7]{1})([1-4]{1})([0-9]{1})))([ ]?)((([0-9]{2})([ ]?)([0-9]{2})([ ]?)([0-9]{2}))|(([0-9]{3})([ ]?)([0-9]{3}))))\" style=\"padding-left: 48px;\" /><br />
+          <div class="form-field">
+            <label for="whatsapp" class="field-title">Teléfono (Whatsapp)</label><br />
+            <script src="/js/main.js"> </script> <label id="tlf-prefijo">
+              <textarea type="text" id="whatsapp" name="whatsapp" class="ibacor_fi" data-prefix="" autocomplete="off" pattern="(((([6]{1})([0-9]{2}))|(([7]{1})([1-4]{1})([0-9]{1})))([ ]?)((([0-9]{2})([ ]?)([0-9]{2})([ ]?)([0-9]{2}))|(([0-9]{3})([ ]?)([0-9]{3}))))" style="padding-left: 48px;"><?php$resultado["telefono"]?></textarea><br />
             </label>
           </div>
 
-          <div class=\"form-field\">
-            <label for=\"perfil\" class=\"field-title\">Foto perfil</label><br />
-            <input type=\"file\" id=\"perfil\" name=\"perfil\" autocomplete=\"off\" accept=\"image/*\" /><br />
-          </div>
-          <input type=\"submit\" value=\"Enviar\" />
+          <div class="form-field">
+            <label for="perfil" class="field-title">Foto perfil</label><br />
+            <input type="file" id="perfil" name="perfil" autocomplete="off" accept="image/*" /><br />
+          </div> <input type="submit" value="Enviar" />
       </div>
       </form>
     </div>
   </div>
-  <!-- Content End -->";
-  $link->close();
-  ?>
+  <!--Content End-->
 
 
-  <!-- JQuery -->
-  <script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  < !--JQuery-->
+    < script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+      </script>
 
-  <script src="/js/main.js"></script>
+      <script src="/js/main.js"></script>
 </body>
 
 </html>

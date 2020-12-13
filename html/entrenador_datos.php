dcc47a9aa2233e48ba20901f92127cbc85348a37
CristianDC27 <cristiancasado27@gmail.com>
2020-12-13 19:41:12 +0100

    entrenador datos arreglo

--- html/entrenador_datos.php
+++ html/entrenador_datos.php
@@ -85,11 +85,10 @@
   <!--Content End-->
 
 
-  < !--JQuery-->
-    < script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-      </script>
+  <!--JQuery-->
+  <script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-      <script src="/js/main.js"></script>
+  <script src="/js/main.js"></script>
 </body>
 
 </html>

857b9431faf36a4478e43f7317ca368e996db6b4
CristianDC27 <cristiancasado27@gmail.com>
2020-12-13 17:28:53 +0100

    datos

--- html/entrenador_datos.php
+++ html/entrenador_datos.php
@@ -43,53 +43,53 @@
   $sql = "SELECT * FROM `Entrenador` WHERE idEntrenador= $idEntrenador";
   $resultado = $link->query($sql);
   mysqli_close($link);
-  ?>
 
+  echo "
   <!-- Content Start -->
-  <div class="main">
-    <div class="content-container">
+  <div class=\"main\">
+    <div class=\"content-container\">
       <h1>Modificar datos de entrenador</h1>
-      <div class="form-container">
-        <form action="/php/entrenador_datos.php" method="POST">
-          <div class="form-field">
-            <label for="email" class="field-title">Correo electronico *</label><br />
-            <input type="email" value="<?php $resultado["correoEntrenador"] ?>" id="email" name="email" pattern="([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})" required />
-            <input typer="text" hidden value="<?php echo ("$idEntrenador") ?>" id="idEntrenador" name="idEntrenador"><br />
+      <div class=\"form-container\">
+        <form action=\"/php/entrenador_datos.php\" method=\"POST\">
+          <div class=\"form-field\">
+            <label for=\"email\" class=\"field-title\">Correo electronico *</label><br />
+            <input type=\"email\" value=\"{$resultado["correoEntrenador"]}\" id=\"email\" name=\"email\" pattern=\"([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})\" required />
+            <input typer=\"text\" hidden value=\"$idEntrenador\" id=\"idEntrenador\" name=\"idEntrenador\"><br />
+          </div>
+          <div class=\"form-field\">
+            <label for=\"password\" class=\"field-title\">Contraseña *</label><br />
+            <input type=\"text\" value=\"{$resultado["claveAcceso"]}\" id=\"password\" name=\"password\" pattern=\"([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})\" required /><br />
           </div>
 
-          <div class="form-field">
-            <label for="password" class="field-title">Contraseña *</label><br />
-            <input type="text" value="<?php $resultado["claveAcceso"] ?>" id="password" name="password" pattern="([a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,})" required /><br />
+          <div class=\"form-field\">
+            <label for=\"name\" class=\"field-title\">Nombre completo</label><br />
+            <input type=\"text\" value=\"{$resultado["nombreCompleto"]}\" id=\"name\" name=\"name\" autocomplete=\"off\" pattern=\"[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,96}\" /><br />
           </div>
 
-          <div class="form-field">
-            <label for="name" class="field-title">Nombre completo</label><br />
-            <input type="text" value="<?php $resultado["nombreCompleto"] ?>" id="name" name="name" autocomplete="off" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,96}" /><br />
+          <div class=\"form-field\">
+            <label for=\"dni\" class=\"field-title\">DNI/NIE</label><br />
+            <input type=\"text\" value=\"{$resultado["DNI"]}\" id=\"dni\" name=\"dni\" autocomplete=\"off\" pattern=\"((([x-zX-Z])|([lmLM])|[0-9]){1}([ ]?)(([0-9]){7})([-]?)([a-zA-Z]{1}))\" minlength=\"9\" maxlength=\"11\" /><br />
           </div>
 
-          <div class="form-field">
-            <label for="dni" class="field-title">DNI/NIE</label><br />
-            <input type="text" value="<?php $resultado["DNI"] ?>" id="dni" name="dni" autocomplete="off" pattern="((([x-zX-Z])|([lmLM])|[0-9]){1}([ ]?)(([0-9]){7})([-]?)([a-zA-Z]{1}))" minlength="9" maxlength="11" /><br />
-          </div>
-
-          <div class="form-field">
-            <label for="whatsapp" class="field-title">Teléfono (Whatsapp)</label><br />
-            <script src="/js/main.js"></script>
-            <label id="tlf-prefijo">
-              <input type="text" value="<?php $resultado["telefono"] ?>" id="whatsapp" name="whatsapp" class="ibacor_fi" data-prefix="" autocomplete="off" pattern="(((([6]{1})([0-9]{2}))|(([7]{1})([1-4]{1})([0-9]{1})))([ ]?)((([0-9]{2})([ ]?)([0-9]{2})([ ]?)([0-9]{2}))|(([0-9]{3})([ ]?)([0-9]{3}))))" style="padding-left: 48px;" /><br />
+          <div class=\"form-field\">
+            <label for=\"whatsapp\" class=\"field-title\">Teléfono (Whatsapp)</label><br />
+            <script src=\"/js/main.js\"></script>
+            <label id=\"tlf-prefijo\">
+              <input type=\"text\" value=\"{$resultado["telefono"]}\" id=\"whatsapp\" name=\"whatsapp\" class=\"ibacor_fi\" data-prefix=\"\" autocomplete=\"off\" pattern=\"(((([6]{1})([0-9]{2}))|(([7]{1})([1-4]{1})([0-9]{1})))([ ]?)((([0-9]{2})([ ]?)([0-9]{2})([ ]?)([0-9]{2}))|(([0-9]{3})([ ]?)([0-9]{3}))))\" style=\"padding-left: 48px;\" /><br />
             </label>
           </div>
 
-          <div class="form-field">
-            <label for="perfil" class="field-title">Foto perfil</label><br />
-            <input type="file" id="perfil" name="perfil" autocomplete="off" accept="image/*" /><br />
+          <div class=\"form-field\">
+            <label for=\"perfil\" class=\"field-title\">Foto perfil</label><br />
+            <input type=\"file\" id=\"perfil\" name=\"perfil\" autocomplete=\"off\" accept=\"image/*\" /><br />
           </div>
-          <input type="submit" value="Enviar" />
+          <input type=\"submit\" value=\"Enviar\" />
       </div>
       </form>
     </div>
   </div>
-  <!-- Content End -->
+  <!-- Content End -->";
+  ?>
 
 
   <!-- JQuery -->

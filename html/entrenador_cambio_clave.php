12728a016e9d7e77537d94b5fe2aae5a493b3919
dimipyv <diego.payn@gmail.com>
2020-12-15 00:12:15 +0100

    fixnamefield

--- html/entrenador_cambio_clave.php
+++ html/entrenador_cambio_clave.php
@@ -77,9 +77,9 @@
         <div class="form-group" id="password-group">
             <?php
             if (empty($contraseña_err)) {
-              echo "<input type=\"password\" class=\"form-control\" placeholder=\"contraseña\" name=\"contraseña\" />";
+              echo "<input type=\"password\" class=\"form-control\" placeholder=\"Contraseña\" name=\"contraseña\" />";
             } else {
-              echo "<input type=\"password\" class=\"form-control is-invalid\" placeholder=\"contraseña\" name=\"contraseña\" />";
+              echo "<input type=\"password\" class=\"form-control is-invalid\" placeholder=\"Contraseña\" name=\"contraseña\" />";
               echo "<div class=\"invalid-feedback\">";
               echo $contraseña_err;
               echo "</div>";
@@ -94,7 +94,7 @@
 
           <div class="form-field">
             <label for="confirm" class="field-title">Confirmar contraseña</label><br />
-            <input class="newPass" type="password" id="confirm" name="confirm" placeholder ="Confiramr contraseña" autocomplete="off"><br />
+            <input class="newPass" type="password" id="confirm" name="confirm" placeholder ="Confirmar contraseña" autocomplete="off"><br />
           </div>
 
           <div class="alertChangePass" style="display: none;">

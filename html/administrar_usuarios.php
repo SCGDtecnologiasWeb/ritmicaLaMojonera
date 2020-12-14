1ae609b7bb5634f12af132ab94683920aa46447a
Gonzalo <gonsalero@gmail.com>
2020-12-15 00:34:53 +0100

    administrar usuarios gimnasta

--- html/administrar_usuarios.php
+++ html/administrar_usuarios.php
@@ -35,10 +35,11 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true || $_SESSI
 
   <!-- Content Start -->
   <div class="content-container">
-    <h1>Entrenadores</h1>
     <?php
+    echo "<h1>Entrenadores</h1>";
+
     require_once($_SERVER['DOCUMENT_ROOT'] . '/php/config.php');
-    $consulta_SQL = "SELECT `idEntrenador`, `correoEntrenador`, `nombreCompleto`, `DNI`, `telefono` FROM Entrenador";
+    $consulta_SQL = "SELECT `idEntrenador`, `correoEntrenador`, `nombreCompleto`, `DNI`, `telefono` FROM `Entrenador`";
     $resultado = $link->query($consulta_SQL);
 
     while ($fila = $resultado->fetch_array()) {
@@ -70,143 +71,56 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true || $_SESSI
       echo      "</div>";
       echo    "</div>";
       echo    "<a href=\"modificar_entrenador.php?idEntrenador={$fila["idEntrenador"]}\">";
-      echo      "<img src=\"/assets/edit-icon.PNG\" class=\"edit-img\" alt=\"Editar entrenador\" />";
+      echo      "<img src=\"/assets/edit-icon.PNG\" class=\"edit-img\" alt=\"Editar entrenador\">";
       echo    "</a>";
       echo    "<a href=\"eliminar_entrenador.php?idEntrenador={$fila["idEntrenador"]}\">";
-      echo      "<img src=\"/assets/bin-icon.PNG\" class=\"bin-img\" alt=\"Eliminar entrenador\" />";
+      echo      "<img src=\"/assets/bin-icon.PNG\" class=\"bin-img\" alt=\"Eliminar entrenador\">";
+      echo    "</a>";
+      echo  "</div>";
+    }
+
+    echo "<h1 class=\"gymnast-header\">Gimnastas</h1>";
+
+    $consulta_SQL = "SELECT `dni`, `nombreCompleto`, `fechaNacimiento`, `nombreTutor`, `telefono`, `nivel`, `consentimientoFotos`, `alergias`, `pago`, `registrado`, `Grupo_idGrupo` FROM `Gimnasta`";
+    $resultado = $link->query($consulta_SQL);
+    while ($fila = $resultado->fetch_array()) {
+
+      echo  "<div class=\"gymnast-container\">";
+      echo    "<div class=\"gymnast\">";
+      echo      "<div class=\"img-container\">";
+      echo        "<img src=\"/assets/matriculaciones/gimnasta_generico.jpg\" alt=\"Gimnasta\">";
+      echo      "</div>";
+      echo      "<div class=\"name-container\">";
+      echo        "<h2>{$fila["nombreCompleto"]}</h2>";
+      echo        "<p>Grupo: {$fila["nivel"]}</p>";
+      echo        "<p class=\"hidden-content hidden\">Consentimiento a fotos: {$fila["consentimientoFotos"]}</p>";
+      echo        "<p class=\"hidden-content hidden\">Pago realizado: {$fila["pago"]} ";
+      if ($fila["pago"] === "Si") {
+        echo          "<a href=\"/assets/matriculaciones/{$fila["dni"]}.jpg\">";
+        echo            "<i class=\"fas fa-external-link-square-alt\"></i>";
+        echo          "</a>";
+      }
+      echo        "</p>";
+      echo      "</div>";
+      echo      "<div class=\"contact-container\">";
+      echo        "<p>Tutor/a legal: {$fila["nombreTutor"]}</p>";
+      echo        "<p>Teléfono de contacto: {$fila["telefono"]}</p>";
+      echo        "<p class=\"hidden-content hidden\">Alergias: {$fila["alergias"]}</p>";
+      echo        "<p class=\"hidden-content hidden\">DNI: {$fila["dni"]}</p>";
+      echo        "<p class=\"hidden-content hidden\">Fecha de nacimiento: {$fila["fechaNacimiento"]}</p>";
+      echo      "</div>";
+      echo    "</div>";
+      echo    "<a href=\"modificar_gimnasta.html?idGimnasta={$fila["dni"]}\">";
+      echo      "<img src=\"/assets/edit-icon.PNG\" class=\"edit-img\" alt=\"Editar\">";
+      echo    "</a>";
+      echo    "<a href=\"eliminar_gimnasta.html?idGimnasta={$fila["dni"]}\">";
+      echo      "<img src=\"/assets/bin-icon.PNG\" class=\"bin-img\" alt=\"Eliminar\">";
       echo    "</a>";
       echo  "</div>";
     }
 
     $link->close();
     ?>
-    <div class="trainer-container">
-      <div class="trainer">
-        <div class="img-container">
-          <img src="/assets/Juanma - profile.jpeg" alt="Entrenador" />
-        </div>
-        <div class="name-container">
-          <h2>Nombre completo</h2>
-          <p class="hidden-content hidden">Entrena: Grupo A</p>
-        </div>
-        <div class="contact-container">
-          <p>Correo eléctronico: ejemploejemploejemplo@gmail.com</p>
-          <p>Teléfono: 666 66 66 66</p>
-          <p class="hidden-content hidden">DNI: 99966699</p>
-        </div>
-      </div>
-      <a href="modificar_usuario.html">
-        <img src="/assets/edit-icon.PNG" class="edit-img" alt="Editar" />
-      </a>
-      <img src="/assets/bin-icon.PNG" class="bin-img" alt="Eliminar" />
-    </div>
-
-
-    <h1 class="gymnast-header">Gimnastas</h1>
-    <div class="gymnast-container">
-      <div class="gymnast">
-        <div class="img-container">
-          <img src="/assets/Juanma - profile.jpeg" alt="Gimnasta" />
-        </div>
-        <div class="name-container">
-          <h2>Nombre completo</h2>
-          <p>Grupo: A</p>
-          <p class="hidden-content hidden">Fecha de nacimiento: XX/XX/XXXX</p>
-        </div>
-        <div class="contact-container">
-          <p>
-            Correo eléctronico: ejemploejemploejemploejemploejemplo@gmail.com
-          </p>
-          <p>Teléfono: 666 66 66 66</p>
-          <p class="hidden-content hidden">
-            Teléfono secundario: 777 77 77 77
-          </p>
-          <p class="hidden-content hidden">DNI: 99966699</p>
-        </div>
-      </div>
-      <a href="modificar_usuario.html">
-        <img src="/assets/edit-icon.PNG" class="edit-img" alt="Editar" />
-      </a>
-      <img src="/assets/bin-icon.PNG" class="bin-img" alt="Eliminar" />
-    </div>
-    <div class="gymnast-container">
-      <div class="gymnast">
-        <div class="img-container">
-          <img src="/assets/Juanma - profile.jpeg" alt="Gimnasta" />
-        </div>
-        <div class="name-container">
-          <h2>Nombre completo</h2>
-          <p>Grupo: A</p>
-          <p class="hidden-content hidden">Fecha de nacimiento: XX/XX/XXXX</p>
-        </div>
-        <div class="contact-container">
-          <p>
-            Correo eléctronico: ejemploejemploejemploejemploejemplo@gmail.com
-          </p>
-          <p>Teléfono: 666 66 66 66</p>
-          <p class="hidden-content hidden">
-            Teléfono secundario: 777 77 77 77
-          </p>
-          <p class="hidden-content hidden">DNI: 99966699</p>
-        </div>
-      </div>
-      <a href="modificar_usuario.html">
-        <img src="/assets/edit-icon.PNG" class="edit-img" alt="Editar" />
-      </a>
-      <img src="/assets/bin-icon.PNG" class="bin-img" alt="Eliminar" />
-    </div>
-    <div class="gymnast-container">
-      <div class="gymnast">
-        <div class="img-container">
-          <img src="/assets/Juanma - profile.jpeg" alt="Gimnasta" />
-        </div>
-        <div class="name-container">
-          <h2>Nombre completo</h2>
-          <p>Grupo: A</p>
-          <p class="hidden-content hidden">Fecha de nacimiento: XX/XX/XXXX</p>
-        </div>
-        <div class="contact-container">
-          <p>
-            Correo eléctronico: ejemploejemploejemploejemploejemplo@gmail.com
-          </p>
-          <p>Teléfono: 666 66 66 66</p>
-          <p class="hidden-content hidden">
-            Teléfono secundario: 777 77 77 77
-          </p>
-          <p class="hidden-content hidden">DNI: 99966699</p>
-        </div>
-      </div>
-      <a href="modificar_usuario.html">
-        <img src="/assets/edit-icon.PNG" class="edit-img" alt="Editar" />
-      </a>
-      <img src="/assets/bin-icon.PNG" class="bin-img" alt="Eliminar" />
-    </div>
-    <div class="gymnast-container">
-      <div class="gymnast">
-        <div class="img-container">
-          <img src="/assets/Juanma - profile.jpeg" alt="Gimnasta" />
-        </div>
-        <div class="name-container">
-          <h2>Nombre completo</h2>
-          <p>Grupo: A</p>
-          <p class="hidden-content hidden">Fecha de nacimiento: XX/XX/XXXX</p>
-        </div>
-        <div class="contact-container">
-          <p>
-            Correo eléctronico: ejemploejemploejemploejemploejemplo@gmail.com
-          </p>
-          <p>Teléfono: 666 66 66 66</p>
-          <p class="hidden-content hidden">
-            Teléfono secundario: 777 77 77 77
-          </p>
-          <p class="hidden-content hidden">DNI: 99966699</p>
-        </div>
-      </div>
-      <a href="modificar_usuario.html">
-        <img src="/assets/edit-icon.PNG" class="edit-img" alt="Editar" />
-      </a>
-      <img src="/assets/bin-icon.PNG" class="bin-img" alt="Eliminar" />
-    </div>
   </div>
   <!-- Content End -->
 

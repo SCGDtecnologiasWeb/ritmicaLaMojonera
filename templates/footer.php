1dfca7e05b0180b23581d338666b8de50967aa6c
CristianDC27 <cristiancasado27@gmail.com>
2020-12-17 13:43:26 +0100

    foter

--- templates/footer.php
+++ templates/footer.php
@@ -5,10 +5,10 @@
       <h1>Ven a visitarnos</h1>
       <p>Concreta una cita y disfruta de nuestros entrenamientos</p>
       <p>
-        <nav><a>
-            C/ Av. Europa, 143 (Carpa de usos multiples), La Mojonera-04745,
-            Almería
-          </a></nav>
+        <a>
+          C/ Av. Europa, 143 (Carpa de usos multiples), La Mojonera-04745,
+          Almería
+        </a>
       </p>
     </div>
     <div class="col-4 footer-col">
@@ -18,17 +18,17 @@
         encantado de atenderte
       </p>
       <p>
-        Correo: <nav><a>cdrlamojonera@gmail.com</a><br /></nav>
-        Telefono: <nav><a>621 013 265</a><br /></nav>
-        Whatsapp: <nav><a>+34 621 013 265</a><br /></nav>
+        Correo: <a>cdrlamojonera@gmail.com</a><br />
+        Telefono: <a>621 013 265</a><br />
+        Whatsapp: <a>+34 621 013 265</a><br />
       </p>
     </div>
     <div class="col-4 footer-col">
       <h1>Siguenos en redes sociales</h1>
       <p>Entérate de todo al instante</p>
-      <nav><a href="https://www.facebook.com/ritmicalamojonera" class="fab fa-facebook-square"></a></nav>
-      <nav><a href="https://www.youtube.com/channel/UC1t4MldjalCTkdC2cYVgVfg" class="fab fa-youtube"></a></nav>
-      <nav><a href="https://www.instagram.com/ritmicalamojonera/?hl=es" class="fab fa-instagram-square"></a></nav>
+      <a href="https://www.facebook.com/ritmicalamojonera" class="fab fa-facebook-square"></a>
+      <a href="https://www.youtube.com/channel/UC1t4MldjalCTkdC2cYVgVfg" class="fab fa-youtube"></a>
+      <a href="https://www.instagram.com/ritmicalamojonera/?hl=es" class="fab fa-instagram-square"></a>
     </div>
   </div>
   <div class="row">

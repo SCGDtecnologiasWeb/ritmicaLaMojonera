15e2bce0ce5fffdd0b94542ae270bf10bc879f6d
Gonzalo <gonsalero@gmail.com>
2020-12-17 15:07:52 +0100

    arreglo

--- templates/header_admin.php
+++ templates/header_admin.php
@@ -1,5 +1,5 @@
 <!-- Header Start -->
 <div class="administrator-header">
-  <img src="/assets/logo_ritmica2.png" alt="Logo/>
+  <img src="/assets/logo_ritmica2.png" alt="Logo" />
 </div>
 <!-- Header End -->

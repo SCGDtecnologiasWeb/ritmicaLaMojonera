3cb6a261a73bbdc51bff8a3d10255528dbda7afb
Gonzalo <gonsalero@gmail.com>
2020-12-19 17:10:26 +0100

    contraseña

--- html/administrador_cambio_clave.php
+++ html/administrador_cambio_clave.php
@@ -1,3 +1,76 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true || $_SESSION["tipo_usuario"] !== "administrador") {
+  header("location: /html/login.php");
+  exit;
+}
+?>
+
+<?php
+$oldPass = "";
+$newPass = "";
+$confirmPass = "";
+$newPass_err = "";
+$oldPass_err = "";
+$confirmPass_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $oldPass = $_POST["oldPass"];
+  $newPass = $_POST["newPass"];
+  $confirmPass = $_POST["confirmPass"];
+
+  // Errores en la contraseña antigua
+  if (strlen($oldPass) === 0) {
+    $oldPass_err .= "No has introducido tu contraseña antigua<br>";
+  }
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/php/config.php');
+  $sql_get_hash = "SELECT `claveAccesoAdmin` FROM `Administrador` WHERE `idAdministrador` = {$_SESSION["id"]}";
+  $resultado = mysqli_query($link, $sql_get_hash);
+  $fila = mysqli_fetch_assoc($resultado);
+  $old_hash = $fila["claveAccesoAdmin"];
+  if (!password_verify($oldPass, $old_hash)) {
+    $oldPass_err .= "La contraseña es incorrecta<br>";
+    mysqli_close($link);
+  }
+
+  // Errores en la nueva contraseña
+  if (strlen($newPass) === 0) {
+    $newPass_err .= "No has introducido tu nueva contraseña<br>";
+  }
+  if (strlen($newPass) < 8) {
+    $newPass_err .= "La contraseña debe tener al menos 8 caracteres<br>";
+  }
+  if (strlen($newPass) > 20) {
+    $newPass_err .= "La contraseña no puede sobrepasar 20 caracteres<br>";
+  }
+  if (!preg_match('/[a-zA-Z]/', $newPass)) {
+    $newPass_err .= "La contraseña debe tener al menos una letra<br>";
+  }
+  if (!preg_match('/\d/', $newPass)) {
+    $newPass_err .= "La contraseña debe contener al menos un número<br>";
+  }
+
+  // Errores en la confirmación de contraseña
+  if (strlen($confirmPass) === 0) {
+    $confirmPass_err .= "No has confirmado tu nueva contraseña<br>";
+  }
+  if ($confirmPass !== $newPass) {
+    $confirmPass_err .= "Las contraseñas no coinciden<br>";
+  }
+
+  if (empty($oldPass_err) && empty($newPass_err) && empty($confirmPass_err)) {
+    $new_hash = password_hash($newPass, PASSWORD_DEFAULT);
+
+    $sql_update_pass = "UPDATE `Administrador` SET `claveAccesoAdmin` = '{$new_hash}' WHERE `idAdministrador` = {$_SESSION["id"]}";
+    mysqli_query($link, $sql_update_pass);
+    mysqli_close($link);
+    header("location: /html/administrar_usuarios.php");
+    exit;
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -6,6 +79,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Cambio contraseña</title>
 
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <!-- FontAwesome CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 
@@ -15,60 +90,6 @@
 </head>
 
 <body>
-  <?php
-  // Comienza la sesión
-  session_start();
-
-  if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true && $_SESSION["tipo_usuario"] === "administrador") {
-    echo "<p style='float: right'>";
-    print_r($_SESSION);
-    echo "</p>";
-  } else {
-    header("location: /html/login.php");
-  }
-
-  $administrador = $_SESSION["id"];
-  // Comprobamos si el campo de contraseña esta vacio
-  if (empty(trim($_POST["contraseña"]))) {
-    $contraseña_err = "Por favor introduce tu contraseña.";
-  } else {
-    $contraseña = trim($_POST["contraseña"]);
-  }
-  if (empty(trim($_POST["new"]))) {
-    $contraseña_err = "Por favor introduce tu contraseña.";
-  } else {
-    $contraseña_nueva = password_hash(trim($_POST["new"]), PASSWORD_DEFAULT);
-  }
-  // $contraseña = password_hash($_POST["contraseña"], PASSWORD_DEFAULT);
-  $contraseña_err = "";
-  // preparamos consulta
-  $sql = "SELECT claveAccesoAdministrador FROM Administrador WHERE idAdministrador = $administrador";
-  if ($stmt = mysqli_query($link, $sql)) {
-    mysqli_stmt_bind_result($stmt, $clave_acceso_administrador);
-    if (mysqli_stmt_execute($stmt)) {
-      mysqli_stmt_store_result($stmt);
-      if (mysqli_stmt_fetch($stmt)) {
-        if (password_verify($contraseña, $clave_acceso_administrador)) {
-          $sql2 = "UPDATE Administrador SET claveAccesoAdministrador=$contraseña_nueva WHERE claveAccesoAdministrador = $clave_acceso_administrador";
-          mysqli_stmt_close($stmt);
-        } else {
-          // Mensaje de error si la contraseña es incorrecta
-          if (empty($contraseña_err)) {
-            $contraseña_err = "La contraseña es incorrecta.";
-          }
-        }
-      }
-
-      mysqli_close($link);
-    } else {
-      mysqli_query($link, "ROLLBACK");
-      mysqli_close($link);
-      header("location: /html/error.php");
-      exit;
-    }
-  }
-  ?>
-
   <?php
   include_once($_SERVER['DOCUMENT_ROOT'] . "/templates/header_admin.php");
 
@@ -83,26 +104,41 @@
     <div class="content-container">
       <h1>Cambiar contraseña</h1>
       <div class="form-container">
-        <form action="#" method="POST" enctype="multipart/form-data">
-          <div class="form-field">
-            <label for="contraseña" class="field-title">Contraseña actual</label><br />
-            <input class="oldPass" type="password" id="oldPass" name="oldPass" placeholder="Contraseña actual" autocomplete="off"><br />
+        <form action="/html/administrador_cambio_clave.php" method="POST" enctype="multipart/form-data">
+
+          <div class="mb-3">
+            <label for="contraseña" class="form-label">Contraseña actual</label>
+            <input class="form-control <?php if (!empty($oldPass_err)) echo "is-invalid"; ?>" type="password" id="oldPass" name="oldPass" required>
+            <div class="invalid-feedback">
+              <?php
+              echo $oldPass_err;
+              ?>
+            </div>
           </div>
 
-          <div class="form-field">
-            <label for="new" class="field-title">Nueva contraseña</label><br />
-            <input class="newPass" type="password" id="new" name="new" placeholder="Nueva contraseña" autocomplete="off"><br />
+          <div class="mb-3">
+            <label for="newPass" class="form-label">Nueva contraseña</label>
+            <input class="form-control <?php if (!empty($newPass_err)) echo "is-invalid"; ?>" type="password" id="newPass" name="newPass" required>
+            <div class="invalid-feedback">
+              <?php
+              echo $newPass_err;
+              ?>
+            </div>
           </div>
 
-          <div class="form-field">
-            <label for="confirm" class="field-title">Confirmar contraseña</label><br />
-            <input class="newPass" type="password" id="confirm" name="confirm" placeholder="Confirmar contraseña" autocomplete="off"><br />
+          <div class="mb-3">
+            <label for="confirmPass" class="form-label">Confirmar contraseña</label>
+            <input class="form-control <?php if (!empty($confirmPass_err)) echo "is-invalid"; ?>" type="password" id="confirmPass" name="confirmPass" required>
+            <div class="invalid-feedback">
+              <?php
+              echo $confirmPass_err;
+              ?>
+            </div>
           </div>
 
-          <div class="alertChangePass" style="display: none;">
-          </div>
+          <div class="alertChangePass" style="display: none;"></div>
 
-          <input type="submit" value="Enviar" />
+          <button type="submit" class="btn btn-primary float-end" id="submitPass">Enviar</button>
 
         </form>
       </div>
@@ -114,6 +150,107 @@
   <script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
   <script src="/js/main.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      // Setup
+      if ($("#oldPass").hasClass("is-invalid")) {
+        oldPassValid = false;
+      } else {
+        validateOldPass();
+      }
+      if ($("#newPass").hasClass("is-invalid")) {
+        newPassValid = false;
+      } else {
+        validateNewPass();
+      }
+      if ($("#confirmPass").hasClass("is-invalid")) {
+        confirmPassValid = false;
+      } else {
+        validateConfirmPass();
+      }
+      validate();
+
+      // Comprobamos cuando se apriete una tecla
+      $("#oldPass").keyup(function() {
+        validateOldPass();
+        validate();
+      });
+
+      $("#newPass").keyup(function() {
+        validateNewPass();
+        validate();
+      });
+
+      $("#confirmPass").keyup(function() {
+        validateConfirmPass();
+        validate();
+      });
+
+      function validateOldPass() {
+        if ($("#oldPass").val().length == 0) {
+          $("#oldPass").removeClass("is-invalid");
+          oldPassValid = false;
+          return;
+        }
+        $("#oldPass").removeClass("is-invalid");
+        oldPassValid = true;
+      }
+
+      function validateNewPass() {
+        if ($("#newPass").val().length == 0) {
+          $("#newPass").removeClass("is-invalid");
+          newPassValid = false;
+          return;
+        } else if ($("#newPass").val().length < 8) {
+          $("#newPass").addClass("is-invalid");
+          $("#newPass").next().html("La contraseña debe tener al menos 8 caracteres");
+          newPassValid = false;
+          return;
+        } else if ($("#newPass").val().length > 20) {
+          $("#newPass").addClass("is-invalid");
+          $("#newPass").next().html("La contraseña no puede sobrepasar 20 caracteres");
+          newPassValid = false;
+          return;
+        } else if (!/[a-zA-Z]/.test($("#newPass").val())) {
+          $("#newPass").addClass("is-invalid");
+          $("#newPass").next().html("La contraseña debe tener al menos una letra");
+          newPassValid = false;
+          return;
+        } else if (!/\d/.test($("#newPass").val())) {
+          $("#newPass").addClass("is-invalid");
+          $("#newPass").next().html("La contraseña debe contener al menos un número");
+          newPassValid = false;
+          return;
+        }
+        $("#newPass").removeClass("is-invalid");
+        newPassValid = true;
+      }
+
+      function validateConfirmPass() {
+        if ($("#confirmPass").val().length == 0) {
+          $("#confirmPass").removeClass("is-invalid");
+          confirmPassValid = false;
+          return;
+        } else if ($("#confirmPass").val() !== $("#newPass").val()) {
+          $("#confirmPass").addClass("is-invalid");
+          $("#confirmPass").next().html("Las contraseñas no coinciden");
+          confirmPassValid = false;
+          return;
+        }
+        $("#confirmPass").removeClass("is-invalid");
+        confirmPassValid = true;
+      }
+
+      function validate() {
+        if (oldPassValid && newPassValid && confirmPassValid) {
+          $("#submitPass").prop("disabled", false);
+        } else {
+          $("#submitPass").prop("disabled", true);
+        }
+      }
+
+    });
+  </script>
 </body>
 
 </html>

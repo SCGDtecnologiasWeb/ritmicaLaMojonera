822c623cf9ff7df6fcaaed666b9bb7e8aa2b3fa0
CristianDC27 <cristiancasado27@gmail.com>
2020-12-20 11:55:31 +0100

    noticias

--- html/noticias.php
+++ html/noticias.php
@@ -35,11 +35,9 @@
             <h1>Mantente Actualizado</h1>
             <div class=\"\"> 
               <label for=\"fecha-inicio\" class=\"form-label\">Fecha de inicio:</label>
-              <input type=\"date\" id=\"fecha-inicio\" name=\"fecha-inicio\" class=\"form-control\">  
-              <i class=\"glyphicon glyphicon-calendar fa fa-calendar\"></i>
+              <input type=\"date\" id=\"fecha-inicio\" name=\"fecha-inicio\" class=\"form-control\">
               <label for=\"fecha-fin\" class=\"form-label\">Fecha de fin:</label>
-              <input type=\"date\" id=\"fecha-fin\" name=\"fecha-fin\" class=\"form-control\">  
-              <i class=\"glyphicon glyphicon-calendar fa fa-calendar\"></i>
+              <input type=\"date\" id=\"fecha-fin\" name=\"fecha-fin\" class=\"form-control\">
             </div>  
           </div>
           <div class=\"container news-container\">
